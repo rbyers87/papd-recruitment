b50f58437f059eb7c7eefb9e335d1ceaf6ff0d60
Ryan Byers <papd437@PAPD.com>
2024-12-05 14:06:16 -0600

    Description of changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Menu } from 'lucide-react';
-import { DepartmentPatch } from './DepartmentPatch';
+import { DepartmentPatch } from './photos/papdPatch';
 
 export function Header() {
   return (
