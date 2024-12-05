1858d35a6438bde14fc53db8278b82146aecafc2
Ryan Byers <papd437@PAPD.com>
2024-12-05 15:10:05 -0600

    reverted to bolt code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Menu } from 'lucide-react';
-import { DepartmentPatch } from './photos/papdPatch.png';
+import { DepartmentPatch } from './DepartmentPatch';
 
 export function Header() {
   return (
     <header className="bg-blue-900 text-white">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-20">
-        <div>
-            <img src={DepartmentPatch} alt="Patch logo" />
-        </div>
+        <div className="flex items-center space-x-4">
+        <DepartmentPatch imageUrl="./photos/papdPatch.png" />
+
+
+            <span className="text-xl font-bold">Port Arthur Police Department</span>
+          </div>
           <nav className="hidden md:flex space-x-8">
             <a href="#about" className="hover:text-blue-200 transition">About</a>
             <a href="#benefits" className="hover:text-blue-200 transition">Benefits</a>
