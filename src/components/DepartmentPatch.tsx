642250ee8450955a6387d931be88c3dab570f094
Ryan Byers <papd437@PAPD.com>
2024-12-05 14:30:36 -0600

    attempt to fix patch

--- src/components/DepartmentPatch.tsx
+++ src/components/DepartmentPatch.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import photos
 
 interface DepartmentPatchProps {
   imageUrl?: string;
 }
 
-export function DepartmentPatch({ imageUrl }: DepartmentPatchProps) {
+export function DepartmentPatch({ imageUrl="/photos/papdPatch.png" }: DepartmentPatchProps) {
   return (
     <div className="w-16 h-16 flex items-center justify-center bg-white rounded-full overflow-hidden border-2 border-blue-200">
       {imageUrl ? (
